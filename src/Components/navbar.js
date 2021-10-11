d0d85f5dfe76b43ab5a1fc2997d0f9738e6dde52
Jasiel Padrón <madjaze.b1@gmail.com>
2021-10-11 19:02:18 -0400

    componente dentro dos cards responsivo

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -22,7 +22,7 @@ function Navbar() {
 
             <ul className={click ? "nav-menu" : "nav-menu-show"}>
             <Link to="/"> <li onClick={handleClick} >Home</li></Link>
-            <Link to="/services" > <li onClick={handleClick} >Services</li>   </Link>
+            <Link to="/service" > <li onClick={handleClick} >Services</li>   </Link>
             <Link to="/products"> <li onClick={handleClick} >Products</li>   </Link>
             <Link to="/sign-up" > <li onClick={handleClick}  className="nav-link-signUp"><i className="far fa-user-circle"></i>Sign-Up</li> </Link>
             <Link to="/admin-mode"><li onClick={handleClick} className="setup"><i className="fas fa-cog"> Admin/mode</i></li></Link>
